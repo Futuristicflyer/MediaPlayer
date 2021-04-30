df04619d323b34b8aeac87d01b2375b5eb454694
Felix Chavira <fgchavira@yahoo.com>
2021-04-30 01:05:29 -0500

    Added comments to code

--- Media.py
+++ Media.py
@@ -18,6 +18,8 @@ if not os.path.isdir(directory_path): os.mkdir(directory_path)
 os.chdir(directory_path)
 pictures = os.listdir()
 
+
+#<--- Setting up images for buttons --->
 playPic = Image.open("playButton2.jpg")
 resizePhoto = playPic.resize((100,100))
 playPic = ImageTk.PhotoImage(resizePhoto)
@@ -34,28 +36,32 @@ unpausePic = Image.open("unpauseButton2.jpg")
 resizePhoto = unpausePic.resize((100,100))
 unpausePic = ImageTk.PhotoImage(resizePhoto)
 
-directory = askdirectory()
+#<------------------------------------->
+
+
+directory = askdirectory() #Prompts user to choose a directory from which they want their music
 os.chdir(directory)
 songList = os.listdir()
 
-playList = tkr.Listbox(musicPlayer, font="Helvetica 12 bold", bg="black", fg="white", selectmode=tkr.SINGLE)
+playList = tkr.Listbox(musicPlayer, font="Helvetica 12 bold", bg="black", fg="white", selectmode=tkr.SINGLE) #Creates a list box with the songs included in the directory chosen by the user.
+
 
 artworkPresent = False
 for item in songList:
     pos=0
-    if ((item[(len(item)-3):(len(item))]) == 'mp3'):
-        playList.insert(pos, item)
+    if ((item[(len(item)-3):(len(item))]) == 'mp3'): #Checks for only mp3 files as to skip over any other non-audio files
+        playList.insert(pos, item) #If file is audio file, it will be addeed onto the list box
         pos+=1
         pygame.init()
         pygame.mixer.init()
-    if ((item[(len(item)-3):(len(item))]) == ('png' or 'jpg')):
+    if ((item[(len(item)-3):(len(item))]) == ('png' or 'jpg')): #Checks folder to see if there is any artwork to be displayed while user listens to music
         artworkPresent = True
         artworkPic = Image.open(item)
         resizePhoto = artworkPic.resize((500,500))
         artworkPic = ImageTk.PhotoImage(resizePhoto)
         artwork = tkr.Label(musicPlayer, bg="black", image=artworkPic)
 
-if artworkPresent == False:
+if artworkPresent == False: #If users directory does not contain artwork, use the default image provided
     artworkPresent == True
     directory_path = os.path.join(os.environ['USERPROFILE'],'Documents','Project','Pictures')
     os.chdir(directory_path)
@@ -67,24 +73,22 @@ if artworkPresent == False:
     os.chdir(directory)
 
 
-def play():
-    pygame.mixer.music.load(playList.get(tkr.ACTIVE))
-
-
-def play():
+def play(): #Defines the play command which begins to play music/audio
     pygame.mixer.music.load(playList.get(tkr.ACTIVE))
     var.set(playList.get(tkr.ACTIVE))
     pygame.mixer.music.play()
 
-def stop():
+def stop(): #Defines stop command which completely stops music, and returns to the beginning
     pygame.mixer.music.stop()
 
-def pause():
+def pause(): #Defines pause command which pauses music at the timestamp the command is issued
     pygame.mixer.music.pause()
 
-def unpause():
+def unpause(): #Defines unpause command which resumes music at the timestamp provided by the pause command
     pygame.mixer.music.unpause()
 
+
+#<---Setting up buttons and the commands and pictures to issue --->
 playButton = tkr.Button(musicPlayer, width=100, height=100, image=playPic, command=play, bg="white")
 
 stopButton = tkr.Button(musicPlayer, width=100, height=100, image=stopPic, command=stop, bg="white")
@@ -92,10 +96,11 @@ stopButton = tkr.Button(musicPlayer, width=100, height=100, image=stopPic, comma
 pauseButton = tkr.Button(musicPlayer, width=100, height=100, image=pausePic, command=pause, bg="white")
 
 unpauseButton = tkr.Button(musicPlayer, width=100, height=100, image=unpausePic, command=unpause, bg="white")
+#<---------------------------------------------------------------->
+
 
 var = tkr.StringVar()
-songTitle = tkr.Label(musicPlayer, font="Helvetica 12 bold", textvariable=var)
-playing = False
+songTitle = tkr.Label(musicPlayer, font="Helvetica 12 bold", textvariable=var) #Sets the name of the song currently playing to the top of the GUI
 
 songTitle.pack()
 artwork.pack()
